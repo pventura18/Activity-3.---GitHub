29f7954f030f22a7a31a53f23197501c6a9f7744
marso3 <martirb11@gmail.com>
2023-01-23 17:52:45 +0100

    menu messages

--- Activity 3. - GitHub/Program.cs
+++ Activity 3. - GitHub/Program.cs
@@ -3,6 +3,16 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Main menu");
+        Console.WriteLine("Press 1 for tobacco");
+        Console.WriteLine("Press 2 for beer");
+        Console.WriteLine("Press 3 or vodka");
+        Console.WriteLine("Press 4 for coffee");
+        Console.WriteLine("Press 5 for football");
+        Console.WriteLine("Press 6 or tattoos");
+        Console.WriteLine("Press 7 for dolphins");
+        Console.WriteLine("Press 8 for basketball");
+        Console.WriteLine("Press 9 or tennis");
+        Console.WriteLine("Press 0 to exit");
     }
 }
