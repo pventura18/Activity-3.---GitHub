e9f75e16d1960e1913f50ef19e8b7951fe81d10b
Pol Ventura Pont <polventurapont@gmail.com>
2023-01-23 18:27:37 +0100

    Menu Ben Parit

--- Activity 3. - GitHub/Program.cs
+++ Activity 3. - GitHub/Program.cs
@@ -11,6 +11,19 @@ internal class Program
 
             tecla = Console.ReadKey();
             Console.Clear();
+
+            Console.WriteLine("Main menu");
+            Console.WriteLine("Press 1 for tobacco");
+            Console.WriteLine("Press 2 for beer");
+            Console.WriteLine("Press 3 or vodka");
+            Console.WriteLine("Press 4 for coffee");
+            Console.WriteLine("Press 5 for football");
+            Console.WriteLine("Press 6 or tattoos");
+            Console.WriteLine("Press 7 for dolphins");
+            Console.WriteLine("Press 8 for basketball");
+            Console.WriteLine("Press 9 or tennis");
+            Console.WriteLine("Press 0 to exit");
+
             switch (tecla.Key)
             {
                 case ConsoleKey.D1:
